b773e6ac07e17b528cb6d13735cd3a8ac31c91b7
Michael Bates <mklbtz@hey.com>
2025-08-12 17:18:15 -0400

    a tongue perhaps

--- frog/froggy.gd
+++ frog/froggy.gd
@@ -6,36 +6,54 @@ extends CharacterBody2D
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var tongue: CharacterBody2D = $Tongue
+@onready var tongueTimer: Timer = $Tongue/Timer
+
+func _ready() -> void:
+	stop_tongue()
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
+	var direction = Input.get_axis(&"move_left", &"move_right")
+	var tongue: bool = Input.is_action_just_pressed(&"tongue")
+	var jump: bool = false # no jump yet
+
+	set_animation_direction(direction, jump)
+
+	if tongue:
+		shoot_tongue()
+	
+	if not is_on_floor(): # no jump yet
 		velocity += get_gravity() * delta
 
-	var left = Input.is_action_pressed(&"move_left")
-	var right = Input.is_action_pressed(&"move_right")
-	var tongue = Input.is_action_just_pressed("tongue")
-	var jump = false
-	var direction = 0
-	
-	if right:
+	velocity.x = direction * speed
+
+	move_and_slide()
+
+
+func stop_tongue():
+	tongue.visible = false
+	tongue.process_mode = Node.PROCESS_MODE_DISABLED
+
+
+func shoot_tongue():
+	tongue.visible = true
+	tongue.process_mode = Node.PROCESS_MODE_PAUSABLE
+	tongue.velocity = Vector2.DOWN
+	tongue.velocity.y *= shoot_velocity
+
+
+func set_animation_direction(direction: int, jump: bool):
+	if direction > 0: # right
 		sprite.flip_h = true
-		direction = 1
-	elif left:
+	elif direction < 0: # left
 		sprite.flip_h = false
-		direction = -1
 
 	# Play animations
 	if is_on_floor() and not jump:
-		if left or right:
-			sprite.play("move")
+		if direction == 0:
+			sprite.play(&"idle")
 		else:
-			sprite.play("idle")
-	
-	# Apply movement
-	if left or right:
-		velocity.x = direction * speed
-	else:
-		velocity.x = 0
-
-	move_and_slide()
+			sprite.play(&"move")
+	else: 
+		sprite.play(&"move") # jumping?
